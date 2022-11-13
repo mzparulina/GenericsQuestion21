a54978027b50ef91d75821716ab75670072c4afd
mzparulina <pauline.revilla@gmail.com>
2022-11-12 19:25:37 -0500

    Initial Commit

---
+++ GenericsQuestion21/MyGenericClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace GenericsQuestion21
+{
+	public class MyGenericClass<T> where T: struct
+    {
+        // define an array of Generic type with length 5
+        private T[] arr = new T[5];
+        int count = 0;
+
+        // adding items mechanism into generic type
+        public void Add(T item)
+        {
+            //checking length
+            if (count + 1 < 6)
+            {
+                arr[count] = item;
+
+            }
+            count++;
+        }
+
+        //indexer for foreach statement iteration
+        public T this[int index]
+        {
+            get { return arr[index]; }
+
+            set { arr[index] = value; }
+        }
+
+        // sort descending
+
+    }
+}
+
---
+++ GenericsQuestion21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsQuestion21
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate generics with Integer
+            MyGenericClass<int> num= new MyGenericClass<int>();
+
+            //adding integer values into collection
+            num.Add(2);
+            num.Add(4);
+            num.Add(3);
+            num.Add(5);
+            num.Add(1);
+
+            //setting the indexes
+            for (int i = 5 - 1; i >= 0; i--)
+            {
+                Console.WriteLine(num[i]);
+            }
+            //returns an item
+            int val = num[1];
+            Console.WriteLine("print a " + val);
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
